6acd820b081924bb4d151474d1d48d7966b80c96
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2025-08-09 16:51:47 +0300

    Margins adjusted

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,7 +8,7 @@ type IntroProps = {
 export default function Intro({ sidebarExpanded, isDesktop }: IntroProps) {
 
     return (
-        <div className={`mt-[18.35%] lg:transition-[margin-left] lg:duration-300 lg:ease-in-out max-sm:ml-[20%] sm:ml-[30%] mr-[50px] md:ml-[35%] ${sidebarExpanded ? 'lg:ml-[49%]' : 'lg:ml-[42%]'}`}>
+        <div className={`mt-[18.35%] lg:transition-[margin-left] lg:duration-300 lg:ease-in-out max-sm:ml-[16%] sm:ml-[30%] mr-[50px] md:ml-[35%] ${sidebarExpanded ? 'lg:ml-[49%]' : 'lg:ml-[42%]'}`}>
           <div className={`transition-[opacity] duration-300 ease-in-out`}>
               <div className={`${isDesktop ? 'flex justify-center items-center' : 'text-center'} mb-[4px]`}>
                   <img className={`${!isDesktop && 'mb-[20%]'} max-lg:ml-[35%] lg:w-[60px] lg:h-[60px] sm:w-[100px] sm:h-[100px] w-[80px] h-[80px] pb-[10px]`} src="/favicon.svg" alt="Logo"/>
@@ -16,7 +16,7 @@ export default function Intro({ sidebarExpanded, isDesktop }: IntroProps) {
                     Welcome to Libra.
                   </h1>
               </div>
-              <p className="max-sm:text-[14px] text-center">Start a new chat or select an existing one</p>
+              <p className="max-sm:text-[14px] text-center max-sm:px-[8px]">Start a new chat or select an existing one</p>
           </div>
       </div>
     )
