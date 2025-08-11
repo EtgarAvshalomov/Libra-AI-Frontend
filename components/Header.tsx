7b8262610170632399e66e0592bddf293dc4b657
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2025-08-11 21:08:59 +0300

    Enforced styles for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,11 +11,11 @@ export default function Header({ messages }: HeaderProps) {
         <div className={`transition-[opacity] duration-300 ease-in-out max-sm:mb-[190px] ${messages && messages.length > 0 ? 'opacity-0' : 'opacity-100'}`}>
             <header className="flex justify-center items-center">
                 <img className="w-[60px] h-[60px] pb-[10px]" src="/favicon.svg" alt="Logo"/>
-                <h1 className="text-[24px] font-bold ml-[8px] mb-[0px]">
+                <h1 className="text-[24px] text-[#ffffff] font-bold ml-[8px] mb-[0px]">
                 Welcome to Libra.
                 </h1>
             </header>
-            <p className="text-[14px] mb-[16px] mt-[8px]">
+            <p className="text-[14px] text-[#ffffff] mb-[16px] mt-[8px]">
             How can we help you today?
             </p>
         </div>
