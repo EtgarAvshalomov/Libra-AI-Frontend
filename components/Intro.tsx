ce74e28e50cb1925b8dfa4c7592b0106fa7100c0
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2025-08-11 20:31:31 +0300

    Mobile margins adjusted

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,7 +8,7 @@ type IntroProps = {
 export default function Intro({ sidebarExpanded, isDesktop }: IntroProps) {
 
     return (
-        <div className={`mt-[18.35%] lg:transition-[margin-left] lg:duration-300 lg:ease-in-out max-sm:ml-[16%] sm:ml-[30%] mr-[50px] md:ml-[35%] ${sidebarExpanded ? 'lg:ml-[49%]' : 'lg:ml-[42%]'}`}>
+        <div className={`mt-[18.35%] lg:transition-[margin-left] lg:duration-300 lg:ease-in-out max-sm:ml-[14%] sm:ml-[30%] mr-[50px] md:ml-[35%] ${sidebarExpanded ? 'lg:ml-[49%]' : 'lg:ml-[42%]'}`}>
           <div className={`transition-[opacity] duration-300 ease-in-out`}>
               <div className={`${isDesktop ? 'flex justify-center items-center' : 'text-center'} mb-[4px]`}>
                   <img className={`${!isDesktop && 'mb-[20%]'} max-lg:ml-[35%] lg:w-[60px] lg:h-[60px] sm:w-[100px] sm:h-[100px] w-[80px] h-[80px] pb-[10px]`} src="/favicon.svg" alt="Logo"/>
