55c9fd7bcebcd78da18db6110f47afc79bd9bfc3
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2025-08-27 22:42:57 +0300

    Minor styling adjusted

--- components/ModelSelect.tsx
+++ components/ModelSelect.tsx
@@ -22,7 +22,7 @@ export default function ModelSelect({ models, selectedModelValue, setSelectedMod
         <div className="flex mt-[3px] ml-[2%] w-[30%]">
             <img className="w-[28px] h-[28px] mr-[10px] mb-[5px]" src="/model.svg" alt="Model" title="Model"/>
             <select 
-                className="bg-transparent max-lg:text-[12px] appearance-none cursor-pointer text-[#cfcfcf] border-1 border-[grey] rounded-[6px] w-[10rem] px-[6px] text-[14px] font-[inter] truncate focus:outline-none focus:bg-[#404045]" 
+                className="bg-transparent max-lg:text-[12px] appearance-none cursor-pointer text-[#cfcfcf] border-1 border-[grey] rounded-[6px] w-[10rem] pl-[6px] text-[14px] font-[inter] truncate focus:outline-none focus:bg-[#404045]" 
                 id="model-select" 
                 value={selectedModelValue}
                 onChange={(event) => updateUserData(event.target.value)} name="model">
